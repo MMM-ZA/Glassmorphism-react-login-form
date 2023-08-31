9264a9918cb38de7228dfc27b3fbf1ed6b00c192
makoma mabilo <makoma44@gmail.com>
2023-08-31 22:20:21 +0800

    added validations to form and added styling

--- login-form/src/Components/SimpleInput.js
+++ login-form/src/Components/SimpleInput.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 const SimpleInput = (props) => {
    const [enteredName,setEnteredName] = useState('');
-  //  const [enteredNameisValid, setEnteredNameisValid] = useState(true);
+   const [enteredNameisValid, setEnteredNameisValid] = useState(false);
 
 
    const nameInputChangeHandler = event => {
@@ -12,18 +12,30 @@ const SimpleInput = (props) => {
    const submitFormHandler = (event) => {
     event.preventDefault();
 
+
+    if (enteredName.trim() === '') {
+       setEnteredNameisValid(false);
+    return;
+    }
+
+    setEnteredNameisValid(true)
+
     console.log(enteredName)
+
 
 
    };
 
+   const nameInputClasses = enteredNameisValid ? 'form-control' : 'form-control invalid';
 
   return (
     <form onSubmit={submitFormHandler}>
-      <div className='form-control'>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input type='text' id='name' onChange={nameInputChangeHandler} />
       </div>
+
+      {!enteredNameisValid && <p className='error-text'>Name can't be empty!</p>}
       <div className="form-actions">
         <button>Submit</button>
       </div>
