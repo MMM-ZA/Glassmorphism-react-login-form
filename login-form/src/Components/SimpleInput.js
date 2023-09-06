f1eee2a816c85814015beae9f31d4e50113acf96
makoma mabilo <makoma44@gmail.com>
2023-09-06 21:54:27 +0800

    edited validations

--- login-form/src/Components/SimpleInput.js
+++ login-form/src/Components/SimpleInput.js
@@ -1,17 +1,37 @@
-import { useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 const SimpleInput = (props) => {
 
-   const nameInputRef = useRef();
    const [enteredName,setEnteredName] = useState('');
    const [enteredNameisValid, setEnteredNameisValid] = useState(false);
 
    const [enetedNameisTouched, setEnteredNameisTouched] = useState(false);
 
+   useEffect(() => {
+
+    if (enteredNameisValid) {
+      console.log('Input is Valid')
+    }
+
+   }, [enteredNameisValid]);
+
+
+
 
    const nameInputChangeHandler = event => {
     setEnteredName(event.target.value);
    };
+
+   const nampeInputBlurHandler = event => {
+    setEnteredNameisTouched(true)
+
+
+    if (enteredName.trim() === '') {
+       setEnteredNameisValid(false);
+    return;
+    }
+
+   }
 
    const submitFormHandler = (event) => {
     event.preventDefault();
@@ -27,8 +47,8 @@ const SimpleInput = (props) => {
 
     console.log(enteredName)
 
-    const eneteredValue = nameInputRef.current.value;
-    console.log(eneteredValue)
+    // const eneteredValue = nameInputRef.current.value;
+    // console.log(eneteredValue)
 
     setEnteredName('');
 
@@ -42,10 +62,11 @@ const SimpleInput = (props) => {
     <form onSubmit={submitFormHandler}>
       <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input ref={nameInputRef}
+        <input
          type='text'
          id='name'
          onChange={nameInputChangeHandler}
+         onBlur={nampeInputBlurHandler}
          value={enteredName}
         />
       </div>
